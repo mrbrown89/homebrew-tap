59532ee4334f5574162c3193a41ced20753a6f41
Matt Brown <mbrown_89@icloud.com>
2026-06-19 14:49:26 +0100

    Updated formula name

--- Formula/workbrew-cli.rb
+++ Formula/workbrew-cli.rb
@@ -2,7 +2,7 @@ class WorkbrewCli < Formula
   desc "CLI for querying Workbrew workspaces"
   homepage "https://github.com/mrbrown89/workbrew-cli"
   url "https://github.com/mrbrown89/workbrew-cli/releases/download/v0.1.0/workbrew-cli-darwin-arm64.tar.gz"
-  sha256 "<new checksum>"
+  sha256 "cf5908b84d2b10b649192e6baf4cf66550128fbaf2eae25cdfbcc22065ccf1ed"
   version "0.1.0"
 
   def install
