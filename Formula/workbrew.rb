0b2ea64eafaa0940947a1a90504ab3de8bdd869f
Matt Brown <mbrown_89@icloud.com>
2026-06-19 14:40:53 +0100

    Changed app name

--- Formula/workbrew.rb
+++ Formula/workbrew.rb
@@ -1,15 +1,15 @@
-class Workbrew < Formula
+class WorkbrewCli < Formula
   desc "CLI for querying Workbrew workspaces"
-  homepage "https://github.com/mattb/workbrew-cli"
-  url "https://github.com/mrbrown89/workbrew-cli/releases/download/v0.1.0/workbrew-darwin-arm64.tar.gz"
-  sha256 "57ca887974ec6ac8a399559cdc9e67418d3d384b6232a3af5753370bcec23ad9"
+  homepage "https://github.com/mrbrown89/workbrew-cli"
+  url "https://github.com/mrbrown89/workbrew-cli/releases/download/v0.1.0/workbrew-cli-darwin-arm64.tar.gz"
+  sha256 "<new checksum>"
   version "0.1.0"
 
   def install
-    bin.install "workbrew"
+    bin.install "workbrew-cli"
   end
 
   test do
-    system "#{bin}/workbrew", "version"
+    system "#{bin}/workbrew-cli", "version"
   end
 end
