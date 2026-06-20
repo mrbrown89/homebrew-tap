8d8bfd95a375c5e3d245cfc140c7b369190ae88f
Matt Brown <mbrown_89@icloud.com>
2026-06-20 09:50:14 +0100

    Updated for the new version

--- Formula/workbrew-cli.rb
+++ Formula/workbrew-cli.rb
@@ -1,9 +1,9 @@
 class WorkbrewCli < Formula
   desc "CLI for querying Workbrew workspaces"
   homepage "https://github.com/mrbrown89/workbrew-cli"
-  url "https://github.com/mrbrown89/workbrew-cli/releases/download/v0.1.1/workbrew-cli-darwin-arm64.tar.gz"
-  sha256 "6a3e4d0b361dd217fadf10dde310ce409a5a59cad11ad5031f468ac2a362cdcd"
-  version "0.1.1"
+  url "https://github.com/mrbrown89/workbrew-cli/releases/download/v0.2.0/workbrew-cli-darwin-arm64.tar.gz"  
+  sha256 "a498546599a1e305d7ffce012c520c98add8b3fac42568ce865f710b1b56ae53"
+  version "0.2.0"
 
   def install
     bin.install "workbrew-cli"
